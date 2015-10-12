102985cb1380e33a0f02506fe2e716f48801ac10
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-10-12 13:30:36 +0300

    fixed commented code:
    1. Removed the clean up
    2. Removed the "test" word from the test methods
    3. Used hamrcrest for the assertions
    4. Added secondary assertions in case of hard-coded values for the
    result
    5. Restored old output stream after the test finishes it's use of the
    new one

--- src/array/ArrayTest.java
+++ src/array/ArrayTest.java
@@ -8,53 +8,70 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by krasimir94 on 10.10.15.
  */
 public class ArrayTest {
-    Array arr = null;
+  Array arr = null;
 
-    @Before
-    public void setUp() {
-        arr = new Array();
-    }
+  @Before
+  public void setUp() {
+    arr = new Array();
+  }
 
-    @After
-    public void tearDown() {
-        arr = null;
-    }
 
-    @Test
-    public void testGetMin() throws Exception {
-        int[] ints = {3, 4, 1, 6, 5};
-        assertEquals("min element not found", 1, arr.getMin(ints));
-    }
+  @Test
+  public void GetMin() throws Exception {
+    int[] ints = {3, 4, 1, 6, 5};
+    int[] ints2 = {1, 2, 3, -5};
+    assertThat("min element not found", 1, is(equalTo(arr.getMin(ints))));
+    assertThat("min element not found correctly", -5, is(equalTo(arr.getMin(ints2))));
+  }
 
-    @Test
-    public void testGetSum() throws Exception {
-        int[] ints = {3, 4, 1, 6, 5};
-        assertEquals("sum not found", 19, arr.getSum(ints));
-    }
+  @Test
+  public void GetSum() throws Exception {
+    int[] ints = {3, 4, 1, 6, 5};
+    int[] ints2 = {1, 1, 1};
+    assertThat("sum not found", 19, is(equalTo(arr.getSum(ints))));
+    assertThat("sumn not found correctly", 3, is(equalTo(arr.getSum(ints2))));
+  }
 
-    @Test
-    public void testPrintAll() throws Exception {
-        OutputStream os = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(os);
-        String newline = System.getProperty("line.separator");
-        System.setOut(ps);
-        int[] ints = {1, 2, 3};
-        arr.printAll(ints);
-        String message = os.toString();
-        assertEquals("print failed", "1, 2, 3, " + newline, message);
-    }
+  @Test
+  public void PrintAll() throws Exception {
+    PrintStream original = System.out;
+    OutputStream os = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(os);
+    System.setOut(ps);
+    String newline = System.getProperty("line.separator");
+    int[] ints = {1, 2, 3};
+    int[] ints2 = {3, 4, 5};
+    arr.printAll(ints);
+    String message = os.toString();
+    assertThat("print failed", "1, 2, 3, " + newline, is(equalTo(message)));
+    os = new ByteArrayOutputStream();
+    ps = new PrintStream(os);
+    System.setOut(ps);
+    arr.printAll(ints2);
+    String message2 = os.toString();
+    System.out.println(message2);
+    assertThat("print incorrect", "3, 4, 5, " + newline, is(equalTo(message2)));
+    System.setOut(original);
+    System.out.println();
+  }
 
-    @Test
-    public void testArrRevert() throws Exception {
-        int[] ints = {1, 2, 3};
-        int[] expected = {3, 2, 1};
-        arr.arrRevert(ints);
-        assertArrayEquals("revertion failed", expected, ints);
-    }
+  @Test
+  public void ArrRevert() throws Exception {
+    int[] ints = {1, 2, 3};
+    int[] expected = {3, 2, 1};
+    int[] ints2 = {9, 8, 0};
+    int[] expected2 = {0, 8, 9};
+    arr.arrRevert(ints);
+    assertThat("revertion failed", expected, is(equalTo(ints)));
+    arr.arrRevert(ints2);
+    assertThat("revertion not correct", expected2, is(equalTo(ints2)));
+  }
 }

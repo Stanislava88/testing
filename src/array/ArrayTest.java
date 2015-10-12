90dca5042546f8eb61e417742871ace3a988b535
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-10-12 14:58:15 +0300

    use Parameterized and used @After to set the OutputStream

--- src/array/ArrayTest.java
+++ src/array/ArrayTest.java
@@ -1,76 +1,88 @@
 package array;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
- * Created by krasimir94 on 10.10.15.
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
  */
+@RunWith(Parameterized.class)
 public class ArrayTest {
-  Array arr = null;
+  Array arr;
+  public final PrintStream original = System.out;
 
   @Before
   public void setUp() {
     arr = new Array();
   }
 
+  @After
+  public void tearDown() {
+    System.setOut(original);
+  }
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+  }
+
+  private int firstDigit;
+  private int secondDigit;
+  private int thirdDigit;
+
+  public ArrayTest(int firstDigit, int secondDigit, int thirdDigit) {
+    this.firstDigit = firstDigit;
+    this.secondDigit = secondDigit;
+    this.thirdDigit = thirdDigit;
+  }
+
 
   @Test
   public void getMin() throws Exception {
-    int[] ints = {3, 4, 1, 6, 5};
-    int[] ints2 = {1, 2, 3, -5};
-    assertThat("min element not found", 1, is(equalTo(arr.getMin(ints))));
-    assertThat("min element not found correctly", -5, is(equalTo(arr.getMin(ints2))));
+    int[] ints = {firstDigit, secondDigit, thirdDigit};
+    assertThat("min element not found", firstDigit, is(equalTo(arr.getMin(ints))));
   }
 
   @Test
   public void getSum() throws Exception {
-    int[] ints = {3, 4, 1, 6, 5};
-    int[] ints2 = {1, 1, 1};
-    assertThat("sum not found", 19, is(equalTo(arr.getSum(ints))));
-    assertThat("sumn not found correctly", 3, is(equalTo(arr.getSum(ints2))));
+    int[] ints = {firstDigit, secondDigit, thirdDigit};
+    int sum = firstDigit + secondDigit + thirdDigit;
+    assertThat("sum not found", sum, is(equalTo(arr.getSum(ints))));
   }
 
   @Test
-  public void printAll() throws Exception {
-    PrintStream original = System.out;
+  public void printAll() {
     OutputStream os = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(os);
     System.setOut(ps);
     String newline = System.getProperty("line.separator");
-    int[] ints = {1, 2, 3};
-    int[] ints2 = {3, 4, 5};
+    int[] ints = {firstDigit, secondDigit, thirdDigit};
     arr.printAll(ints);
     String message = os.toString();
-    assertThat("print failed", "1, 2, 3, " + newline, is(equalTo(message)));
-    os = new ByteArrayOutputStream();
-    ps = new PrintStream(os);
-    System.setOut(ps);
-    arr.printAll(ints2);
-    String message2 = os.toString();
-    System.out.println(message2);
-    assertThat("print incorrect", "3, 4, 5, " + newline, is(equalTo(message2)));
-    System.setOut(original);
-    System.out.println();
+    String expected = firstDigit + ", " + secondDigit + ", " + thirdDigit + ", " + newline;
+    assertThat("print failed", expected, is(equalTo(message)));
   }
 
   @Test
-  public void arrRevert() throws Exception {
-    int[] ints = {1, 2, 3};
-    int[] expected = {3, 2, 1};
-    int[] ints2 = {9, 8, 0};
-    int[] expected2 = {0, 8, 9};
+  public void arrRevert() {
+    int[] ints = {firstDigit, secondDigit, thirdDigit};
+    int[] expected = {thirdDigit, secondDigit, firstDigit};
     arr.arrRevert(ints);
     assertThat("revertion failed", expected, is(equalTo(ints)));
-    arr.arrRevert(ints2);
-    assertThat("revertion not correct", expected2, is(equalTo(ints2)));
+
   }
 }
